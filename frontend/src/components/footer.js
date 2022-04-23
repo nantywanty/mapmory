a319688c1d7c6a62716e87a4a24058cbbc6da452
nantywanty <nant.arun.etc@gmail.com>
2022-04-23 18:26:44 +0000

    added footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -12,7 +12,7 @@ import SimpleReactFooter from "simple-react-footer";
 
 function Footer() {
 
-    const description = "Created by Nant and Tomohisa as a project for the NUS IT5007 course, MAPMORY seeks to redefine how photos are stored and displayed, allowing one to relive their favourite adventures in an intuitive and visual manner any time, anywhere. Try it for free today!";
+    const description = "Created by Nant and Tomohisa as a project for the NUS IT5007 course, MAPMORY seeks to redefine how photos are stored and displayed. You can now relive their your adventures in an intuitive and visual manner any time, anywhere. Try it for free today!";
     const title = "About Mapmory";
     const columns = [
         // {
